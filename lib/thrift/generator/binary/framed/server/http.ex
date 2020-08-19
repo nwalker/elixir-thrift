f27083b7c4897d5b7810052ef6cd1b58b898c614
Alexsandr Tortsev <ecialo@yandex.ru>
2020-08-19 20:09:14 +0300

    WIP

--- lib/thrift/generator/binary/framed/server/http.ex
+++ lib/thrift/generator/binary/framed/server/http.ex
@@ -1,21 +1,24 @@
 defmodule Thrift.Generator.Binary.Framed.Server.HTTP do
-  def generate(dest_module, service, file_group) do
+  alias Thrift.Generator.{
+    Service,
+  }
+  def generate(service_module, service, file_group) do
 
-    thrift_root = generate_thrift_root(service)
+    thrift_root = generate_thrift_root(service_module, service)
 
     quote do
       defmodule Binary.Framed.Server.HTTP do
 
         unquote(thrift_root)
 
-        def get_child_spec do
-          {Plug.Cowboy, scheme: :http, plug: __MODULE__.Router, options: [port: 4001]}
+        def get_child_spec(module_handler) do
+          {Plug.Cowboy, scheme: :http, plug: {__MODULE__.Router, [module_handler]}, options: [port: 4001]}
         end
       end
     end
   end
 
-  def generate_thrift_root(service) do
+  def generate_thrift_root(service_module, service) do
     quote do
       defmodule Router do
         use Plug.Router
@@ -23,7 +26,15 @@ defmodule Thrift.Generator.Binary.Framed.Server.HTTP do
         alias Thrift.Protocol
 
         plug :match
-        plug :dispatch
+        plug :dispatch, builder_opts()
+
+        # def init([module_handler]) do
+        #   module_handler
+        # end
+
+        def init(opts) do
+          IO.inspect(opts, label: "INIT OPTS")
+        end
 
         post "/thrift" do
           # send_resp(conn, 200, "keke")
@@ -31,7 +42,8 @@ defmodule Thrift.Generator.Binary.Framed.Server.HTTP do
             {:ok, payload, conn} <- Plug.Conn.read_body(conn),
             {:ok, parsed} <- Protocol.Binary.deserialize(:message_begin, payload) |> IO.inspect()
           ) do
-            handle_thrift_message(conn, parsed)
+            IO.inspect(opts, label: "THRIFT OPTS")
+            handle_thrift_message(conn, parsed, opts)
           end
         end
 
@@ -39,19 +51,20 @@ defmodule Thrift.Generator.Binary.Framed.Server.HTTP do
           send_resp(conn, 404, "not found")
         end
 
-        def handle_thrift_message(conn, {:call, sequence_id, name, args_binary}) do
+        def handle_thrift_message(conn, {:call, sequence_id, name, args_binary}, opts) do
           IO.inspect(name, label: "FORWARD")
+          IO.inspect(opts, label: "FORWARD opts")
           conn = %{conn | body_params: args_binary, path_info: [name]}
-          Plug.forward(conn, [name], __MODULE__.Handlers, [])
+          Plug.forward(conn, [name], __MODULE__.Handlers, opts)
         end
 
-        unquote(generate_thrift_handlers(service))
+        unquote(generate_thrift_handlers(service_module, service))
 
       end
     end
   end
 
-  def generate_thrift_handlers(service) do
+  def generate_thrift_handlers(service_module, service) do
     quote do
       defmodule Handlers do
         use Plug.Router
@@ -59,7 +72,11 @@ defmodule Thrift.Generator.Binary.Framed.Server.HTTP do
         plug :match
         plug :dispatch
 
-        unquote_splicing(Enum.map(service.functions, &generate_thrift_handle/1))
+        def init([opts]) do
+          opts
+        end
+
+        unquote_splicing(Enum.map(service.functions, &generate_thrift_handle(service_module, &1)))
 
         match _ do
           IO.inspect(conn, label: "Not matched")
@@ -69,11 +86,33 @@ defmodule Thrift.Generator.Binary.Framed.Server.HTTP do
     end
   end
 
-  def generate_thrift_handle({fname, _function_ast}) do
-    func_name = "/" <> Atom.to_string(fname)
+  def generate_thrift_handle(service_module, {fname, function_ast}) do
+    func_name = Atom.to_string(fname)
+    func_path = "/" <> func_name
+
+    handle =
+      func_name
+      |> Macro.underscore()
+      |> String.to_atom()
+    handler_args = Enum.map(function_ast.params, &Macro.var(&1.name, nil))
+    args_module = Module.concat(service_module, Service.module_name(function_ast, :args))
+
+    struct_matches =
+      Enum.map(function_ast.params, fn param ->
+        {param.name, Macro.var(param.name, nil)}
+      end)
     quote do
-      post unquote(func_name) do
-        send_resp(conn, 200, unquote(func_name) <> "GOTCHA")
+      post unquote(func_path) do
+        with(
+          {:ok, body, conn} <- Plug.Conn.read_body(conn),
+          {
+            %unquote(args_module){unquote_splicing(struct_matches)},  # %Service.AddArgs{left: left, right: right}
+            rest
+          } <- unquote(args_module).BinaryProtocol.deserialize(body)
+        ) do
+          
+        end
+        send_resp(conn, 200, unquote(fname) <> "GOTCHA" <> inspect(opts))
       end
     end
 
